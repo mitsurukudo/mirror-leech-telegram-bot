21aa7a32f032ce5443b33c9bc822c971ff78f282
mitsurukudo <106009953+mitsurukudo@users.noreply.github.com>
2022-05-22 15:00:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
+GH_UNAME=mitsurukudo
+GH_REPO=mirror-leech-telegram-bot
+
 wget -q https://github.com/$GH_UNAME/$GH_REPO/raw/master/alive.py
 wget -q https://github.com/$GH_UNAME/$GH_REPO/raw/master/aria.sh
 wget -q https://github.com/$GH_UNAME/$GH_REPO/raw/master/extract
