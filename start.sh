3213df57e8a3e48dccae6d1081a9515f2f163111
mitsurukudo <106009953+mitsurukudo@users.noreply.github.com>
2022-05-22 14:38:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,68 @@
+wget -q https://github.com/$GH_UNAME/$GH_REPO/raw/master/alive.py
+wget -q https://github.com/$GH_UNAME/$GH_REPO/raw/master/aria.sh
+wget -q https://github.com/$GH_UNAME/$GH_REPO/raw/master/extract
+wget -q https://github.com/$GH_UNAME/$GH_REPO/raw/master/pextract
+wget -q https://github.com/$GH_UNAME/$GH_REPO/raw/master/requirements.txt
+
+wget -q -P bot/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/__init__.py
+wget -q -P bot/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/__main__.py
+
+wget -q -P bot/helper/ext_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/ext_utils/bot_utils.py
+wget -q -P bot/helper/ext_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/ext_utils/db_handler.py
+wget -q -P bot/helper/ext_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/ext_utils/exceptions.py
+wget -q -P bot/helper/ext_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/ext_utils/fs_utils.py
+wget -q -P bot/helper/ext_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/ext_utils/shortenurl.py
+wget -q -P bot/helper/ext_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/ext_utils/telegraph_helper.py
+
+wget -q -P bot/helper/mirror_utils/download_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/mirror_utils/download_utils/aria2_download.py
+wget -q -P bot/helper/mirror_utils/download_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/mirror_utils/download_utils/direct_link_generator.py
+wget -q -P bot/helper/mirror_utils/download_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/mirror_utils/download_utils/gd_downloader.py
+wget -q -P bot/helper/mirror_utils/download_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/mirror_utils/download_utils/mega_downloader.py
+wget -q -P bot/helper/mirror_utils/download_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/mirror_utils/download_utils/qbit_downloader.py
+wget -q -P bot/helper/mirror_utils/download_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/mirror_utils/download_utils/telegram_downloader.py
+wget -q -P bot/helper/mirror_utils/download_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/mirror_utils/download_utils/youtube_dl_download_helper.py
+
+wget -q -P bot/helper/mirror_utils/status_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/mirror_utils/status_utils/aria_download_status.py
+wget -q -P bot/helper/mirror_utils/status_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/mirror_utils/status_utils/clone_status.py
+wget -q -P bot/helper/mirror_utils/status_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/mirror_utils/status_utils/extract_status.py
+wget -q -P bot/helper/mirror_utils/status_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/mirror_utils/status_utils/gd_download_status.py
+wget -q -P bot/helper/mirror_utils/status_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/mirror_utils/status_utils/mega_download_status.py
+wget -q -P bot/helper/mirror_utils/status_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/mirror_utils/status_utils/qbit_download_status.py
+wget -q -P bot/helper/mirror_utils/status_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/mirror_utils/status_utils/split_status.py
+wget -q -P bot/helper/mirror_utils/status_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/mirror_utils/status_utils/telegram_download_status.py
+wget -q -P bot/helper/mirror_utils/status_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/mirror_utils/status_utils/tg_upload_status.py
+wget -q -P bot/helper/mirror_utils/status_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/mirror_utils/status_utils/upload_status.py
+wget -q -P bot/helper/mirror_utils/status_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/mirror_utils/status_utils/youtube_dl_download_status.py
+wget -q -P bot/helper/mirror_utils/status_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/mirror_utils/status_utils/zip_status.py
+
+wget -q -P bot/helper/mirror_utils/upload_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/mirror_utils/upload_utils/gdriveTools.py
+wget -q -P bot/helper/mirror_utils/upload_utils/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+
+wget -q -P bot/helper/telegram_helper/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/telegram_helper/bot_commands.py
+wget -q -P bot/helper/telegram_helper/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/telegram_helper/button_build.py
+wget -q -P bot/helper/telegram_helper/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/telegram_helper/filters.py
+wget -q -P bot/helper/telegram_helper/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/helper/telegram_helper/message_utils.py
+
+wget -q -P bot/modules/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/modules/authorize.py
+wget -q -P bot/modules/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/modules/cancel_mirror.py
+wget -q -P bot/modules/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/modules/clone.py
+wget -q -P bot/modules/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/modules/count.py
+wget -q -P bot/modules/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/modules/delete.py
+wget -q -P bot/modules/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/modules/eval.py
+wget -q -P bot/modules/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/modules/leech_settings.py
+wget -q -P bot/modules/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/modules/list.py
+wget -q -P bot/modules/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/modules/mirror.py
+wget -q -P bot/modules/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/modules/mirror_status.py
+wget -q -P bot/modules/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/modules/rss.py
+wget -q -P bot/modules/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/modules/search.py
+wget -q -P bot/modules/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/modules/shell.py
+wget -q -P bot/modules/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/bot/modules/watch.py
+
+wget -q -P qBittorrent/config/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/qBittorrent/config/qBittorrent.conf
+
+wget -q -P web/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/web/nodes.py
+wget -q -P web/ https://github.com/$GH_UNAME/$GH_REPO/raw/master/web/wserver.py
+
+pip3 install --no-cache-dir -r requirements.txt
+
 python3 -m bot
